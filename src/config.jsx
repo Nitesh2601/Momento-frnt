997db4af5417bff68748203f785891d1b2804911
Nitesh <kumarnitesh2601@gmail.com>
2025-03-03 18:37:58 +0530

    Updated frontend changes

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,14 @@
 
+const API_BASE_URL =import.meta.env.VITE_APP_BACKEND_URL;
 
-const API_BASE_URL =
-  import.meta.env.MODE === 'production'
-    ? import.meta.env.VITE_APP_BACKEND_URL
-    : import.meta.env.VITE_APP_LOCAL_BACKEND_URL;
+// const API_BASE_URL =
+//   import.meta.env.MODE === 'production'
+//     ? import.meta.env.VITE_APP_BACKEND_URL
+//     : import.meta.env.VITE_APP_LOCAL_BACKEND_URL;
 
 export default API_BASE_URL;
+
+// console.log('MODE:', import.meta.env.MODE);
+// console.log('VITE_APP_BACKEND_URL:', import.meta.env.VITE_APP_BACKEND_URL);
+// console.log('VITE_APP_LOCAL_BACKEND_URL:', import.meta.env.VITE_APP_LOCAL_BACKEND_URL);
+// console.log('Resolved API_BASE_URL:', API_BASE_URL);
